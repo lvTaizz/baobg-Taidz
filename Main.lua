2e371a9479312abe478ad6bf5b681d2898d59acd
lvTaizz <leviettai182@gmail.com>
2024-11-29 12:54:59 +0700

    Add files via upload

--- Main.lua
+++ Main.lua
@@ -1046,46 +1046,7 @@ end
     end)
     L_93_.CornerRadius = UDim.new(0, 15)
     L_93_.Parent = L_92_
-
-local function playSound()
-	local sound = Instance.new("Sound", game:GetService("CoreGui"))
-	sound.SoundId = "rbxassetid://"
-	sound.Volume = 10
-	sound:Play()
-end
-
-playSound()
-
-repeat
-	wait()
-until game.Players
-repeat
-	wait()
-until game.Players.LocalPlayer
-repeat
-	wait()
-until game.ReplicatedStorage
-repeat
-	wait()
-until game.ReplicatedStorage:FindFirstChild("Remotes");
-repeat
-	wait()
-until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
-repeat
-	wait()
-until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
-repeat
-	wait()
-until game:GetService("Players")
-repeat
-	wait()
-until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Energy")
-wait(0.5)
-if not game:IsLoaded() then
-	repeat
-		game.Loaded:Wait()
-	until game:IsLoaded()
-end--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
     --Remove Effect
     -----------------------------------------------------------
     if game:GetService("ReplicatedStorage").Effect.Container:FindFirstChild("Death") then
